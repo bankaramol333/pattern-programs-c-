19842baefcd6dd4b733f7de157188baad65c1984
bankaramol333 <bankaramol333@gmail.com>
2024-07-15 11:49:34 +0530

    Basic_Square_incrementing_Pattern.

---
+++ Basic_Square_incrementing_Pattern.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main()
+{
+    int n;
+    cout << "Enter value for No. of rows" << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << i + 1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
